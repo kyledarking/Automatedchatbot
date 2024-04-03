d4974c562a1236b33385b253796d08f1d950a07a
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-04-03 23:53:56 +0800

    Update resend.js

--- script/event/resend.js
+++ script/event/resend.js
@@ -1,57 +1,102 @@
 module.exports.config = {
     name: "resend",
     version: "1.0.0",
-    
 };
-var msgData = {} 
+
+const msgData = {};
 
 module.exports.handleEvent = async function ({ api, event }) {
-  if(event.type == 'message') {
-    msgData[event.messageID] = {
-      body: event.body, 
-      attachments: event.attachments
+    if (event.type === 'message') {
+        msgData[event.messageID] = {
+            body: event.body,
+            attachments: event.attachments
+        };
     }
 
-  }
-  if(event.type == "message_unsend" && msgData.hasOwnProperty(event.messageID)) { 
-const info = await api.getUserInfo(event.senderID);
-const name = info[event.senderID].name
+    if (event.type === "message_unsend" && msgData.hasOwnProperty(event.messageID)) {
+        const info = await api.getUserInfo(event.senderID);
+        const name = info[event.senderID].name;
+
+        let messageType = "";
+        let attachmentData = null;
+
+        if (msgData[event.messageID].attachments.length > 0) {
+            const attachment = msgData[event.messageID].attachments[0];
+            attachmentData = await downloadAttachment(attachment);
+
+            switch (attachment.type) {
+                case 'photo':
+                    messageType = "photo";
+                    break;
+                case 'audio':
+                    messageType = "voice message";
+                    break;
+                case 'animated_image':
+                    messageType = "gif";
+                    break;
+                default:
+                    messageType = "message";
+                    break;
+            }
+        } else {
+            messageType = "message";
+        }
+
+        let messageToSend = `Unsend kapang gago ka: "${name}"\eto yung inunsend: ${msgData[event.messageID].body}`;
+
+        if (attachmentData) {
+            api.sendMessage({ body: messageToSend, attachment: attachmentData }, event.threadID, () => {
+                cleanupAttachments(attachmentData);
+            });
+        } else {
+            api.sendMessage(messageToSend, event.threadID);
+        }
+    }
+};
+
+async function downloadAttachment(attachment) {
     const axios = require('axios');
-    const fs = require("fs")
-    if(msgData[event.messageID].attachments.length === 0) {
-        api.sendMessage(`${name} unsent this message: ${msgData[event.messageID].body}`, event.threadID)  
-    } else if(msgData[event.messageID].attachments[0].type == 'photo')  {   
-      var photo = []
-      var del = []
-      for (const item of msgData[event.messageID].attachments) {
-       let { data } = await axios.get(item.url, {responseType: "arraybuffer"})
-          fs.writeFileSync(`./script/cache/${item.filename}.jpg`, Buffer.from(data))
-          photo.push(fs.createReadStream(`./script/cache/${item.filename}.jpg`))
-          del.push(`./script/cache/${item.filename}.jpg`)
-      }
-                    api.sendMessage({body:`${name} unsent this photo: ${msgData[event.messageID].body}`, attachment: photo}, event.threadID, () => {
-               for (const item of del) fs.unlinkSync(item)
-             }) 
+    const fs = require("fs");
+    const path = require("path");
 
-} else if (msgData[event.messageID].attachments[0].type == 'audio') { 
+    let attachmentData = null;
 
-let { data } = await axios.get(msgData[event.messageID].attachments[0].url, {responseType: "arraybuffer"})
+    if (attachment.type === 'photo') {
+        const photo = [];
+        const del = [];
 
- fs.writeFileSync(`./script/cache/audio.mp3`, Buffer.from(data)) 
+        for (const item of msgData[event.messageID].attachments) {
+            const { data } = await axios.get(item.url, { responseType: "arraybuffer" });
+            const filePath = path.resolve(__dirname, "cache", `${item.filename}.jpg`);
+            fs.writeFileSync(filePath, Buffer.from(data));
+            photo.push(fs.createReadStream(filePath));
+            del.push(filePath);
+        }
 
-api.sendMessage({body:`${name} unsent this voice message: ${msgData[event.messageID].body}`, attachment: fs.createReadStream('./script/cache/audio.mp3')}, event.threadID, () => {
-     fs.unlinkSync('./script/cache/audio.mp3')
-             });
-
-    } else if (msgData[event.messageID].attachments[0].type == 'animated_image') {
-
- let { data } = await axios.get(msgData[event.messageID].attachments[0].previewUrl, {responseType: "arraybuffer"})
-
- fs.writeFileSync(`./script/cache/animated_image.gif`, Buffer.from(data)) 
-
-api.sendMessage({body:`${name} unsent this gif: ${msgData[event.messageID].body}`, attachment: fs.createReadStream('./script/cache/animated_image.gif')}, event.threadID, () => {
-     fs.unlinkSync('./script/cache/animated_image.gif')
-             });     
+        attachmentData = photo;
+    } else if (attachment.type === 'audio') {
+        const { data } = await axios.get(attachment.url, { responseType: "arraybuffer" });
+        const filePath = path.resolve(__dirname, "cache", "audio.mp3");
+        fs.writeFileSync(filePath, Buffer.from(data));
+        attachmentData = fs.createReadStream(filePath);
+    } else if (attachment.type === 'animated_image') {
+        const { data } = await axios.get(attachment.previewUrl, { responseType: "arraybuffer" });
+        const filePath = path.resolve(__dirname, "cache", "animated_image.gif");
+        fs.writeFileSync(filePath, Buffer.from(data));
+        attachmentData = fs.createReadStream(filePath);
     }
-  }
+
+    return attachmentData;
+}
+
+function cleanupAttachments(attachments) {
+    const fs = require("fs");
+
+    if (Array.isArray(attachments)) {
+        for (const attachment of attachments) {
+            fs.unlinkSync(attachment.path);
+        }
+    } else {
+        fs.unlinkSync(attachments.path);
+    }
 }
