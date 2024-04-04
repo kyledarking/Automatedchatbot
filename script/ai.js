e3b4c1f38ae95c7b2cbe3f821d6b2febca8e93b9
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-04-05 00:09:16 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 
 module.exports.config = {
-    name: "ai",
+    name: "luci",
     version: "1.0.0",
     credits: "JayMar", // credits to liane cagara!!!!
     role: 0,
-    usage: "ai [prompt]",
+    usage: "luci [prompt]",
     hasPrefix: false,
     cooldowns: 0
 };
@@ -19,7 +19,7 @@ module.exports.run = async function ({ api, event, args }) {
         if (query) {
             api.setMessageReaction("⏳", event.messageID, (err) => console.log(err), true);
             const processingMessage = await api.sendMessage(
-                `🔎 | Asking Churchill (AI). Please wait a moment...`,
+                `🔎 | Asking Chill AI. Please wait a moment...`,
                 event.threadID
             );
 
@@ -31,17 +31,18 @@ module.exports.run = async function ({ api, event, args }) {
                 api.setMessageReaction("✅", event.messageID, (err) => console.log(err), true);
                 await api.sendMessage({ body: trimmedMessage }, event.threadID, event.messageID);
 
-                console.log(`Sent chill (AI)'s response to the user`);
-                await api.sendMessage("The bot was developed by Churchill. 𝗱𝗲𝘃 𝗳𝗯 𝗹𝗶𝗻𝗸: https://www.facebook.com/Churchill.Dev4100", event.threadID);
+                console.log(`Sent Chill AI's response to the user`);
             } else {
-                throw new Error(`Invalid or missing response from chill (AI) API`);
+                throw new Error(`Invalid or missing response from Chill AI API`);
             }
 
             await api.unsendMessage(processingMessage.messageID);
         }
     } catch (error) {
-        console.error(`❌ | Failed to get chill (AI)'s response pls contact churchill to fix this: ${error.message}`);
+        console.error(`❌ | Failed to get Chill AI's response: ${error.message}`);
         const errorMessage = `❌ | An error occurred. You can try typing your query again or resending it. There might be an issue with the server that's causing the problem, and it might resolve on retrying.`;
         api.sendMessage(errorMessage, event.threadID);
+    } finally {
+        await api.sendMessage("The bot was developed by CHURCHILL. Dev fb link: [https://www.facebook.com/Churchill.Dev4100]", event.threadID);
     }
 };
