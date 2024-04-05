294005b0e72db3f8df3814fcfc7b0550109f16e9
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-04-05 19:03:50 +0800

    Update prefix.js

--- script/prefix.js
+++ script/prefix.js
@@ -9,7 +9,7 @@ module.exports.config = {
 		hasPrefix: false,
 		usages: "prefix",
 		cooldown: 5,
-		aliases: ["prefix", "Prefix", "PREFIX", "prefi"],
+		aliases: ["prefix", "Prefix", "PREFIX", "prefix"],
 };
 
 module.exports.run = function ({ api, event, prefix, admin }) {
@@ -26,7 +26,7 @@ module.exports.run = function ({ api, event, prefix, admin }) {
 
 		api.sendMessage(
 				{
-						body: `Yo, my prefix is [ 𓆩 ${prefix} 𓆪 ]\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ ${prefix}help [number of page] -> see commands\n➥ ${prefix}sim [message] -> talk to bot\n➥ ${prefix}callad [message] -> report any problem encountered\n➥ ${prefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\nBot Developer: https://www.facebook.com/${admin}`,
+						body: `Yo, my prefix is [ 𓆩 ${prefix} 𓆪 ]\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ ${prefix}help [number of page] -> see commands\n➥ ${prefix}sim [message] -> talk to bot\n➥ ${prefix}callad [message] -> report any problem encountered\n➥ ${prefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\nBot Developer: https://www.facebook.com/Churchill.Dev4100`,
 						attachment: fs.createReadStream(__dirname + "/cache2/prefix.jpeg")
 				},
 				threadID,
