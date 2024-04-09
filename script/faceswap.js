c20856a7c630d5c6d0cf075e168889a993dab41e
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-04-09 14:33:14 +0800

    Update faceswap.js

--- script/faceswap.js
+++ script/faceswap.js
@@ -1,40 +1,41 @@
-module.exports.config = {
-	name: "faceswap",
-	version: "1.0.0",
-	credits: "Samir",
-	hasPermision: 0,
-	cooldowns: 15,
-	usePrefix: false,
-	description: "Swap faces in two images",
-	commandCategory: "FUN",
-	usage: "",
-};
+module.exports = {
+  config: {
+    name: "faceswap",
+    aliases: ["swap", "exchange"],
+    version: "1.0",
+    author: 'Samir Œ',
+    shortDescription: "Swap faces in two images",
+    longDescription: "Swap faces in two images provided as attachments.",
+    category: "𝗙𝗨𝗡"
+  },
 
-module.exports.run = async function({ args, event, api }) {
-	try {
-		if (event.type != "message_reply") {
-			return api.sendMessage("Please reply to a message with two images attached.", event.threadID);
-		}
+  onStart: async function({ message, event, api }) {
+    try {
+      if (event.type != "message_reply") {
+        return message.reply("Please reply to a message with two images attached.");
+      }
 
-		let links = [];
-		for (let attachment of event.messageReply.attachments) {
-			links.push(attachment.url);
-		}
+      let links = [];
+      for (let attachment of event.messageReply.attachments) {
+        links.push(attachment.url);
+      }
 
-		if (links.length < 2) {
-			return api.sendMessage("Please ensure there are exactly two images attached.", event.threadID);
-		}
+      if (links.length < 2) {
+        return message.reply("Please ensure there are exactly two images attached.");
+      }
 
-		const [Url1, Url2] = await Promise.all(links.map(async link => {
-			const shortLink = await global.utils.uploadImgbb(link);
-			return shortLink.image.url;
-		}));
+      const shortLink1 = await global.utils.uploadImgbb(links[0]);
+      const Url1 = shortLink1.image.url;
 
-		let swapface = `https://apis-samir.onrender.com/faceswap?sourceUrl=${Url1}&targetUrl=${Url2}`;
-		const stream = await global.utils.getStreamFromURL(swapface);
-		api.sendMessage({ attachment: stream }, event.threadID);
-	} catch (error) {
-		console.error(error);
-		api.sendMessage("An error occurred while processing the face swap.", event.threadID);
-	}
+      const shortLink2 = await global.utils.uploadImgbb(links[1]);
+      const Url2 = shortLink2.image.url;
+
+      let swapface = `https://apis-samir.onrender.com/faceswap?sourceUrl=${Url1}&targetUrl=${Url2}`;
+      const stream = await global.utils.getStreamFromURL(swapface);
+      message.reply({ body: "", attachment: stream });
+    } catch (error) {
+      console.error(error);
+      message.reply("An error occurred while processing the face swap.");
+    }
+  }
 };
