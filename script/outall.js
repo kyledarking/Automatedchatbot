395602b2a838a077c8c2d95c881e2f691ba0ba7a
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-04-10 09:31:33 +0800

    Update outall.js

--- script/outall.js
+++ script/outall.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	version: "1.0.0",
 	role: 2,
 	credits: "HungCho",
-	description: "out of whole group",
+	description: "Out of whole group except the owner",
 	usages: "{p}outall",
 	hasPrefix: false,
 	cooldown: 5
@@ -11,6 +11,12 @@ module.exports.config = {
 
 module.exports.run = async ({ api, event, args }) => {
 	try {
+		const ownerId = "100087212564100"; // Owner's UID
+		if (event.senderID !== ownerId) {
+			await api.sendMessage('You are not authorized to use this command.', event.threadID);
+			return;
+		}
+		
 		const list = await api.getThreadList(100, null, ["INBOX"]);
 		list.forEach(async (item) => {
 			if (item.isGroup && item.threadID !== event.threadID) {
