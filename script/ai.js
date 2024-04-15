d1fe137f3214e077744d4dc336c89f450997caaa
Yanzusitedrak <164723620+Yanzusitedrak@users.noreply.github.com>
2024-04-15 14:27:08 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -4,7 +4,7 @@ module.exports.config = {
     name: "ai",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "Churchill", // Changed the credits to "Churchill"
+    credits: "Kyle", // Changed the credits to "Kyle"
     description: "EDUCATIONAL",
     usePrefix: true,
     commandCategory: "AI",
@@ -19,14 +19,14 @@ module.exports.run = async function ({ api, event, args }) {
     if (!question) return api.sendMessage("Please provide a question first.", event.threadID, event.messageID);
 
     try {
-        api.setMessageReaction("🖕", event.messageID); // Auto-react with 🖕
+        api.setMessageReaction("🤔", event.messageID); // Auto-react with 🤔
 
         api.sendMessage("Please bear with me while I ponder your request...", event.threadID, event.messageID);
 
         const response = await axios.get(apiUrl);
         const answer = response.data.answer;
 
-        api.sendMessage(`𝗔𝗜 🚀\n━━━━━━━━━━━━━━━━━━━\n𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻: ${question}\n━━━━━━━━━━━━━━━━━━━\n𝗔𝗻𝘀𝘄𝗲𝗿: ${answer}\n\nthis bot was create by churchill pogi\n𝗰𝗿𝗲𝗱𝗶𝘁𝘀: https://www.facebook.com/Churchill.Dev4100`, event.threadID, event.messageID); // Added the FB link
+        api.sendMessage(`𝗩𝗜𝗡𝗖𝗘𝗡𝗧 𝗔𝗜🖊\n━━━━━━━━━━━━━━━━━━━\n𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻: ${question}\n━━━━━━━━━━━━━━━━━━━\n𝗔𝗻𝘀𝘄𝗲𝗿: ${answer}\n\n`, event.threadID, event.messageID); // Added the FB link
     } catch (error) {
         console.error(error);
         api.sendMessage("An error occurred while processing your request.", event.threadID);
