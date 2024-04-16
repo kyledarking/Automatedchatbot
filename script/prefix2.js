302803e3008abfb970b08eb0887912db6d96a551
kyledarking <167161371+kyledarking@users.noreply.github.com>
2024-04-16 19:35:34 +0800

    Update and rename prefix.js to prefix2.js

--- script/prefix2.js
+++ script/prefix2.js
@@ -26,7 +26,7 @@ module.exports.run = function ({ api, event, prefix, admin }) {
     // Check if the command is invoked manually with the prefix
     if (body.toLowerCase() === `${prefix}prefix`) {
         api.sendMessage(
-            `Hey there! My prefix is [ 𓆩 ${prefix} 𓆪 ].`,
+            `Hey there! My prefix is [ ${prefix} ].`,
             threadID,
             messageID
         );
@@ -36,7 +36,7 @@ module.exports.run = function ({ api, event, prefix, admin }) {
     // Sending the message along with the attachment
     api.sendMessage(
         {
-            body: `Yo, my prefix is [ 𓆩 ${prefix} 𓆪 ]\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ ${prefix}help [number of page] -> see commands\n➥ ${prefix}sim [message] -> talk to bot\n➥ ${prefix}callad [message] -> report any problem encountered\n➥ ${prefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\nBot Developer: https://www.facebook.com/Churchill.Dev4100`,
+            body: `Yo, my prefix is [ ${prefix} ]\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ ${prefix}help [number of page] -> see commands\n➥ ${prefix}sim [message] -> talk to bot\n➥ ${prefix}callad [message] -> report any problem encountered\n➥ ${prefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\nBot Developer: https://www.facebook.com/Churchill.Dev4100`,
             attachment: fs.createReadStream(__dirname + "/cache2/prefix.jpeg")
         },
         threadID,
@@ -53,7 +53,7 @@ module.exports.run = function ({ api, event, prefix, admin }) {
                 threadID,
                 () => {}
             );
-            api.setMessageReaction("🚀", messageInfo.messageID, (err) => {}, true);
+            api.setMessageReaction("🪬", messageInfo.messageID, (err) => {}, true);
         }
     );
 };
