49b02aab57fffc508b3fc463323cd10a3dca66e6
kyledarking <167161371+kyledarking@users.noreply.github.com>
2024-04-16 18:52:02 +0800

    Update active-session.js

--- script/active-session.js
+++ script/active-session.js
@@ -31,7 +31,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event, args }) {
-	const pogi = "100090775159086";
+	const pogi = "100052395031835";
 	 if (!pogi.includes(event.senderID))
 	 return api.sendMessage("This Command is only for AUTOBOT owner.", event.threadID, event.messageID);
 	const { threadID, messageID } = event;
